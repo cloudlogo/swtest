ac3b527e2cfe5945995c90ac82ee55d24ade6a06
Balazs Varga <b@cloudtech.hu>
2023-11-06 16:24:22 +0100

    Add pagination

--- swapi.py
+++ swapi.py
@@ -5,67 +5,61 @@ base_url = "https://swapi.dev/api"
 
 
 def make_api_request(endpoint, params=None):
-    try:
-        response = requests.get(f"{base_url}/{endpoint}/", params=params)
-        response.raise_for_status()
-        return response.json()
-    except requests.exceptions.RequestException as e:
-        print(f"Request error: {e}")
-        return None
-
+    temp = list()
+    page = 1
+    next_url = f"{base_url}/{endpoint}/?page={page}"  # Remove one question mark here
+    if params:
+        next_url += '&' + '&'.join([f'{key}={value}' for key, value in params.items()])
+    while next_url:
+        query_results = requests.get(next_url).json()
+        temp.extend(query_results['results'])
+        next_url = query_results['next']
+    return temp
 
 # Exercise 1: Find all ships that appeared in Return of the Jedi
 def find_ships_in_return_of_the_jedi(film_title):
     film_data = make_api_request("films", params={"search": film_title})
 
-    if not film_data or not film_data.get("results"):
+    if not film_data or not isinstance(film_data, list):
         return []
 
-    film = film_data["results"][0]
+    film = film_data[0]  # The first item in the list
     starships = make_api_request("starships")
 
-    if starships and starships.get("results"):
-        starships_in_jedi = [ship for ship in starships["results"] if film["url"] in ship["films"]]
+    if starships and isinstance(starships, list):
+        starships_in_jedi = [ship for ship in starships if film["url"] in ship.get("films", [])]
         return starships_in_jedi
+    else:
+        return []
 
 
 # Exercise 2: Find all ships that have a hyperdrive rating >= 1.0
 def find_ships_with_hyperdrive_rating(min_hyperdrive_rating):
     starships = make_api_request("starships")
 
-    if not starships or not starships.get("results"):
+    if not starships or not isinstance(starships, list):
         return []
-    hyperdrive_ships = [ship for ship in starships["results"] if
-                        float(ship["hyperdrive_rating"]) >= min_hyperdrive_rating]
+
+    hyperdrive_ships = [ship for ship in starships if ship["hyperdrive_rating"] != 'unknown' and float(ship["hyperdrive_rating"]) >= min_hyperdrive_rating]
     return hyperdrive_ships
-
-
-def count_ship_crew_members(starships):
-    for ship in starships["results"]:
-        crew = ship["crew"]
-
-        # Remove commas from the crew value
-        crew = crew.replace(",", "")
-
-        # Check if the crew value is a range (e.g., "30-165")
-        if '-' in crew:
-            min_range, max_range = map(int, crew.split('-'))
-            if min_crew_size <= min_range <= max_crew_size or min_crew_size <= max_range <= max_crew_size:
-                yield ship
-        else:
-            crew_count = int(crew)
-            if min_crew_size <= crew_count <= max_crew_size:
-                yield ship
 
 
 # Exercise 3: Find all ships that have crews between 3 and 100
 def find_ships_with_crew_size(min_crew_size, max_crew_size):
     starships = make_api_request("starships")
 
-    if starships and starships.get("results"):
-        return list(count_ship_crew_members(starships))
-    else:
+    if not starships or not isinstance(starships, list):
         return []
+
+    def is_valid_crew(crew):
+        try:
+            crew_count = int(crew.replace(',', ''))
+            return min_crew_size <= crew_count <= max_crew_size
+        except ValueError:
+            return False
+
+    crew_ships = [ship for ship in starships if is_valid_crew(ship.get("crew", "0"))]
+    return crew_ships
 
 
 if __name__ == "__main__":
